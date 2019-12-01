31e3640c2047005e69bee701dd62e12a567374c2
Zahid Bilal Özgen <54806367+zbilalozgen@users.noreply.github.com>
2019-12-01 22:23:02 +0300

    Update Filters.js

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,39 +1,44 @@
-import React from 'react';
-import {breedOptions} from "../constants";
+import React from "react";
+import { breedOptions } from "../constants";
 import classNames from "classnames";
 
 function Filters(props) {
-    console.log(props.activeFilter);
-    return (
-        <div>
-            <h4 className="my-4 text-left">Cins</h4>
-            <ul>
-                <li className={
-                    classNames({
-                        "list-group-item": true,
-                        "active": !props.activeFilter,
-                    })
-                } onClick={() => {
-                    props.onFilterPets("");
-                }}>Hepsi</li>
-                {
-                    breedOptions.map((breed) => {
-                        return <li
-                            key={breed}
-                            className={classNames({
-                                "list-group-item": true,
-                                "active": breed === props.activeFilter
-                                }
-                            )}
-                            onClick={(e) => {
-                                e.preventDefault();
-                                props.onFilterPets(breed);
-                            }}>{breed}</li>
-                    })
-                }
-            </ul>
-        </div>
-    );
+  console.log(props.activeFilter);
+  return (
+    <div>
+      <ul>
+        <h4 className="my-4 ">Cins</h4>
+        <li
+          className={classNames({
+            "list-group-item": true,
+            active: !props.activeFilter
+          })}
+          onClick={() => {
+            props.onFilterPets("");
+          }}
+        >
+          Hepsi
+        </li>
+        {breedOptions.map(breed => {
+          return (
+            <li
+              key={breed}
+              className={classNames({
+                "list-group-item": true,
+                active: breed === props.activeFilter
+              })}
+              onClick={e => {
+                e.preventDefault();
+                props.onFilterPets(breed);
+              }}
+            >
+              {breed}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Filters;
