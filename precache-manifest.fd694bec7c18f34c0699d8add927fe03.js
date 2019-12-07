0dd5b4f2d656e1811a7f9dbccb58e8bd090ab092
zbilalozgen <zahidbilalozgen@gmail.com>
2019-12-07 14:15:24 +0300

    Updates

---
+++ precache-manifest.fd694bec7c18f34c0699d8add927fe03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f34c98883b0df8b2a89240cd63778bd",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "070dd793a23a4596def7",
+    "url": "/my-app/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "a07c1b884c0c340c2a17",
+    "url": "/my-app/static/css/main.bb55690d.chunk.css"
+  },
+  {
+    "revision": "070dd793a23a4596def7",
+    "url": "/my-app/static/js/2.2423be32.chunk.js"
+  },
+  {
+    "revision": "a07c1b884c0c340c2a17",
+    "url": "/my-app/static/js/main.9684dee6.chunk.js"
+  },
+  {
+    "revision": "a8ced57b05147e7c287e",
+    "url": "/my-app/static/js/runtime-main.a0a66a6d.js"
+  }
+]);
